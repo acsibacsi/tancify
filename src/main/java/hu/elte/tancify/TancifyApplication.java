35e49f5edee1c6bbd471258773f49155b48d1bf8
Aron <arontheking@gmail.com>
2020-02-27 15:26:07 +0000

    add a maven project template
    add new files generated by Spring Initializr

---
+++ src/main/java/hu/elte/tancify/TancifyApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.tancify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TancifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TancifyApplication.class, args);
+	}
+
+}
